e2cb8acf7ee4e5c96df22e04037280662fd0c299
Mehmet Nur <mkn123@ray25.doc.ic.ac.uk>
2024-01-29 16:02:31 +0000

    first_draft

---
+++ src/main/kotlin/algebra/real/Vector.kt
@@ -0,0 +1,46 @@
+data class Vector(private val doubles: List<Double>) {
+    init {
+        if (doubles.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+    }
+    val length = doubles.size
+    operator fun get(index: Int): Double {
+        if (index > length) {
+            throw IndexOutOfBoundsException()
+        } else {
+            return doubles[index]
+        }
+    }
+    operator fun plus(other: Vector): Vector {
+        if (this.length != other.length) {
+            throw UnsupportedOperationException()
+        } else {
+            return (this + other)
+        }
+    }
+    operator fun times(other: Double): Vector {
+        return Vector(this.doubles.map{ it*other })
+    }
+    infix fun dot(other: Vector): Double {
+        if (this.length != other.length) {
+            throw UnsupportedOperationException()
+        } else {
+            return this.doubles.zip(other.doubles).sumOf { (a, b) -> a * b }
+        }
+    }
+
+    override fun toString(): String {
+        val finalString = StringBuilder()
+        finalString.append("(")
+        this.doubles.forEachIndexed{i,d ->
+            if (i == length -1){
+                finalString.append("$d")
+            } else {
+                finalString.append("$d, ")
+            }
+        }
+        finalString.append(")")
+        return finalString.toString()
+    }
+}
